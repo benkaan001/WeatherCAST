93bfd8f82867830b13a8d2a2b13ef418797ba0f5
Ben Kaan <benkaan001@gmail.com>
2021-09-14 22:19:05 -0500

    js apiURL setup

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,27 @@
 
-api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
-049287381c116e50be8c10a4efd8f47c
+
+var getCity = function(cityname){
+    //  format the 5-day weather forecast API
+    var apiURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + {cityname} + "&appid={049287381c116e50be8c10a4efd8f47c}";
+
+    // make a get request to the url
+
+    fetch(apiURL)
+    .then(function(response){
+        //request was successful
+        if (response.ok) {
+            console.log(response);
+            response.json().then(function(data) {
+                console.log(data);
+                displayCity(data,cityname);
+            });
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    })
+    .catch(function(error) {
+        alert("Unable to connect to Open Weather Map")
+    });
+
+};
+
